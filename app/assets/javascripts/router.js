bfa26b736f38be2b7f025b94bae596c22355b7ea
Brian Douglas <brian.douglas@izea.com>
2014-07-08 23:04:27 -0400

    added work from chapter 10

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,4 +1,8 @@
 // For more information see: http://emberjs.com/guides/routing/
+App.Router.reopen({
+  location: 'auto',
+  rootURL: '/'
+})
 
 App.Router.map(function() {
   // this.resource('posts');
