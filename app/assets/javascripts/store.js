c22284f1234b95a14421fa0f6ec89c56119749e2
Brian Douglas <brian.douglas@izea.com>
2014-07-08 23:18:39 -0400

    added chapter 11 code

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,9 +1,6 @@
-App.Store = DS.Store.extend({
+DS.RESTAdapter.reopen({
+  namespace: 'api/v1'
+})
 
-});
-
-// Override the default adapter with the `DS.ActiveModelAdapter` which
-// is built to work nicely with the ActiveModel::Serializers gem.
-App.ApplicationAdapter = DS.ActiveModelAdapter.extend({
-
-});
+App.Store = DS.Store.extend({});
+App.ApplicationAdapter = DS.ActiveModelAdapter.extend({});
